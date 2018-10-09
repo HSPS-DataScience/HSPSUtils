4b4a85b330a5e85e7bb58874b348a1b9bc657f7b
johnmwood <jmw.home@gmail.com>
2018-10-09 10:49:31 -0600

    Added sample_n_groups to public utils

--- R/utility_public.R
+++ R/utility_public.R
@@ -22,3 +22,34 @@ gather_group_by_count <- function(data) {
     mutate(Count = length(value)) %>%
     slice(1) # slices the first group of data, giving us the final table
 }
+
+#' Gather, group_by the generated key and value, count, then slice
+#'   to create a frequency table of the tibble. Functions with multiple
+#'   variables.
+#'
+#' @param data A tibble
+#' @return A tibble
+#' @import dplyr
+#' @export
+#' @example
+#' mtcars %>%
+#'   sample_n_groups()
+#'
+#'
+sample_n_groups <- function(tbl, size, replace = FALSE, weight = NULL) {
+  # regroup when done
+  grps <- tbl %>%
+    groups() %>%
+    lapply(as.character) %>%
+    unlist()
+  # check length of groups non-zero
+  keep <- tbl %>%
+    summarise() %>%
+    ungroup() %>%
+    sample_n(size, replace, weight)
+  # keep only selected groups, regroup because joins change count.
+  # regrouping may be unnecessary but joins do something funky to grouping variable
+  tbl %>%
+    right_join(keep, by = grps) %>%
+    group_by_(.dots = grps)
+}
